853dac426dbbe7fb2bed735edf5bdc12ea76e4fe
NickGuo <guohao4@lenovo.com>
2017-05-02 17:37:30 +0800

    Add permission checker in AD

--- app/src/main/java/org/newstand/datamigration/ui/activity/AdActivity.java
+++ app/src/main/java/org/newstand/datamigration/ui/activity/AdActivity.java
@@ -1,7 +1,10 @@
 package org.newstand.datamigration.ui.activity;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+
+import com.tbruyelle.rxpermissions2.RxPermissions;
 
 import net.youmi.android.AdManager;
 import net.youmi.android.normal.banner.BannerManager;
@@ -17,6 +20,7 @@ import java.util.List;
 
 import dev.nick.tiles.tile.Category;
 import dev.nick.tiles.tile.DashboardFragment;
+import io.reactivex.functions.Consumer;
 
 /**
  * Created by Nick@NewStand.org on 2017/5/2 16:08
@@ -32,9 +36,29 @@ public class AdActivity extends TransitionSafeActivity {
         showHomeAsUp();
         setTitle(getString(R.string.title_show_ads, EmojiUtils.getEmojiByUnicode(0x1F60F)));
         setContentView(R.layout.activity_with_container_template);
+
+        requestPerms();
+
         AdManager.getInstance(this).init(AdsManager.APP_ID, AdsManager.APP_SECRET, SettingsProvider.isDebugEnabled());
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.container, AdFragment.getInstance()).commit();
+    }
+
+    private void requestPerms() {
+        RxPermissions rxPermissions = new RxPermissions(this);
+        rxPermissions.request(Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.SYSTEM_ALERT_WINDOW
+                )
+                .subscribe(new Consumer<Boolean>() {
+                    @Override
+                    public void accept(Boolean granted) throws Exception {
+                        // Ignored.
+                    }
+                });
     }
 
     public static class AdFragment extends DashboardFragment {
