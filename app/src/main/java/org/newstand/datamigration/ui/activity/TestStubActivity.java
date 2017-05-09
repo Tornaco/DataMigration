12ae2c961f3c93527401de24120e8ad2179d6c62
NickGuo <guohao4@lenovo.com>
2017-05-09 18:09:39 +0800

    Clean up

---
+++ app/src/main/java/org/newstand/datamigration/ui/activity/TestStubActivity.java
@@ -0,0 +1,13 @@
+package org.newstand.datamigration.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nick on 2017/5/8 15:03
+ * E-Mail: Tornaco@163.com
+ * All right reserved.
+ */
+
+public class TestStubActivity extends AppCompatActivity {
+
+}
